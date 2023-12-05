73a1e413b401c2e38da9347985a1202f8af602d4
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2023-12-05 09:29:31 -0500

    feat: Add startup message
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- joinmotd_reforged/__init__.py
+++ joinmotd_reforged/__init__.py
@@ -3,6 +3,7 @@ import os
 from mcdreforged.api.all import *
 
 from joinmotd_reforged.config import Config
+from joinmotd_reforged.utils.version_utils import get_version
 
 Prefix = '!!joinMOTD'
 config: Config
@@ -10,4 +11,15 @@ ConfigFilePath = os.path.join('config', 'joinMOTD-Reforged.json')
 
 
 def on_load(server: PluginServerInterface, old):
-    server.logger.info('Hello world!')
+    global config
+    config = server.load_config_simple(file_name=ConfigFilePath, in_data_folder=False, target_class=Config)
+
+    server.logger.info("==========================================================")
+    server.logger.info("joinMOTD-Reforged is loaded!")
+    plugin_version = get_version()
+    server.logger.info(f"Version: {plugin_version}")
+    if "DEV" in plugin_version or "Alpha" in plugin_version or "Beta" in plugin_version:
+        server.logger.info("§cTHIS IS IN EXPERIMENTAL STAGE, DO NOT USE IN PRODUCTION ENVIRONMENT!")
+    elif "Release Candidate" in plugin_version:
+        server.logger.info("§eTHIS IS A RELEASE CANDIDATE, PLEASE REPORT BUGS TO THE AUTHOR!")
+    server.logger.info("==========================================================")
---
+++ joinmotd_reforged/utils/version_utils.py
@@ -0,0 +1,40 @@
+import json
+import re
+
+from mcdreforged.api.all import PluginServerInterface, ServerInterface
+
+server: PluginServerInterface = ServerInterface.get_instance().as_plugin_server_interface()
+version_properties = None
+
+
+def load_version_properties():
+    global version_properties
+    with server.open_bundled_file(f"joinmotd_reforged/version.json") as f:
+        version_properties = json.load(f)
+
+
+def get_version_property(value: str) -> str:
+    return version_properties[value]
+
+
+def get_version() -> str:
+    version_property = get_version_property("version")
+    revision_property = get_version_property("revision")
+    revision_property = revision_property.upper()
+    stage_property = get_version_property("stage")
+    if stage_property == "stable":
+        version_property = f"{version_property} ({revision_property})"
+        return version_property
+    stage_property = re.sub(r"dev", "DEV", stage_property)
+    stage_property = re.sub(r"alpha\.", "Alpha ", stage_property)
+    stage_property = re.sub(r"alpha", "Alpha", stage_property)
+    stage_property = re.sub(r"beta\.", "Beta ", stage_property)
+    stage_property = re.sub(r"beta", "Beta", stage_property)
+    stage_property = re.sub(r"rc\.", "Release Candidate ", stage_property)
+    stage_property = re.sub(r"rc", "Release Candidate", stage_property)
+    version_property = f"{version_property} {stage_property} ({revision_property})"
+    return version_property
+
+
+def get_build_date() -> str:
+    return get_version_property("build_date")
