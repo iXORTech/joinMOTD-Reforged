cb8c73d098a6ceac00aa2a4ef7b3d7ee5b4ebcb7
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2023-12-05 09:19:18 -0500

    feat: Init Config
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- joinmotd_reforged/__init__.py
+++ joinmotd_reforged/__init__.py
@@ -1,4 +1,12 @@
+import os
+
 from mcdreforged.api.all import *
+
+from joinmotd_reforged.config import Config
+
+Prefix = '!!joinMOTD'
+config: Config
+ConfigFilePath = os.path.join('config', 'joinMOTD-Reforged.json')
 
 
 def on_load(server: PluginServerInterface, old):
